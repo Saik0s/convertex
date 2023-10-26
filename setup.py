d1fb2dd571d638db07c2bf9886143999d1e819ba
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2023-10-26 06:16:16 +0200

    chore: Refactor package name and update version number
    - Change package name from 'Convertex' to 'convertex'
    - Update version from '0.1.0' to '0.1.1'

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Convertex',
-    version='0.1.0',
+    name='convertex',
+    version='0.1.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
